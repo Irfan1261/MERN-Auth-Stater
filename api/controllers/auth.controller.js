3aa27da5a4432c53d12e65a0961a1a519b67fa4c
Irfan Arif <irfanarif444@gmail.com>
2024-08-20 23:14:19 +0500

    Add middleware for better error handling

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,30 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      next(errorHandler(400, "All fields are required"));
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.log("Error in signup controller: ", error);
+    next(error);
+  }
+};
