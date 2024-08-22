6714544c44c1c8b95aab9202042a5778f027c9e8
Irfan Arif <irfanarif444@gmail.com>
2024-08-22 23:11:08 +0500

    Create the sigin controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -25,6 +26,43 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     console.log("Error in signup controller: ", error);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === "" || password === "") {
+      next(errorHandler(400, "All fields are required"));
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      next(errorHandler(402, "Wrong credentials"));
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      next(errorHandler(401, "Wrong credentials"));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    //for excluding password from response
+    const { password: pass, ...rest } = user._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    console.log("Error in signin controller: ", error);
     next(error);
   }
 };
